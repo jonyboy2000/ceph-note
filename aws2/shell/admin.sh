e9a9ced958e246ce5fc99279d8984c334e38195f
yuliyang <yuliyang@cmss.chinamobile.com>
2017-09-09 08:42:58 +0800

    curl version demo for rgw admin rest api

---
+++ aws2/shell/admin.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+KEY_ACCESS="yly"
+KEY_SECRET="yly"
+relativePath="/admin/usage"
+cmd="${relativePath}?format=json&uid=yly"
+current=`TZ=GMT LANG=en_US date "+%a, %d %b %Y %H:%M:%S GMT"`
+stringToSign="GET\n\n\n${current}\n${relativePath}"
+signature=`echo -en ${stringToSign} | openssl sha1 -hmac ${KEY_SECRET} -binary | base64`
+HOST="127.0.0.1"
+
+curl -s -v -X GET "http://${HOST}${cmd}" \
+-H "Authorization: AWS ${KEY_ACCESS}:${signature}" \
+-H "Date: ${current}" \
+-H "Host: ${HOST}"
